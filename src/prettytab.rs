01c419f60763eb430f4e4a6479d4bce3831637f9
christhirst <chrisgluon@gmail.com>
2024-01-02 10:18:58 +0100

    fix

--- src/prettytab.rs
+++ src/prettytab.rs
@@ -1,4 +1,4 @@
-use prettytable::{format, row, Cell, Row, Table};
+use prettytable::{format, Cell, Row, Table};
 
 fn rowfromstr(table: &mut Table, data: Vec<Vec<&str>>) {
     for row in data {
@@ -10,7 +10,7 @@ fn rowfromstr(table: &mut Table, data: Vec<Vec<&str>>) {
 
 pub fn printastab(title: Vec<&str>, data: Vec<Vec<&str>>) {
     let mut table = Table::new();
-    let format = format::FormatBuilder::new()
+    let _format = format::FormatBuilder::new()
         .column_separator('|')
         .borders('|')
         .separators(
@@ -31,12 +31,9 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        let hay = "kihubertmueller@schnipp.de";
-        let want = "hubertmueller@schnipp.de";
-        let reg = r"^ki";
         let title = vec!["dn", "attr", "regex", "replace", "Before", "After"];
         let data: Vec<Vec<&str>> = vec![vec!["1", "2"], vec!["3", "4"], vec!["6", "q"]];
-        let result = printastab(title, data);
+        let _result = printastab(title, data);
         //assert_eq!(result, want);
     }
 }
